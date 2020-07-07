b912542a52f9f3f00f99ac7aa42c9ca10743dbbb
andrei232000 <andreimaria23@yahoo.de>
2020-07-07 12:52:11 +0100

    Academically Correct Version

--- index.ts
+++ index.ts
@@ -1,54 +1,148 @@
 let i : number;
 let text : string;
-let fezz : string;
 
-for(i = 1; i < 1000; i++)
+interface Rule
+{
+    rule_id : number;
+    isConditionTrue(n : number) : boolean;
+    apply(text : string) : string;
+}
+
+const rule3 : Rule =
+{
+    rule_id : 3,
+
+    isConditionTrue(n : number) : boolean
+    {
+        return (n % 3 == 0);
+    },
+
+    apply(text : string) : string
+    {
+        return (text + "Fizz");
+    }
+}
+
+const rule5 : Rule =
+{
+    rule_id : 5,
+
+    isConditionTrue(n : number) : boolean
+    {
+        return (n % 5 == 0);
+    },
+
+    apply(text : string) : string
+    {
+        return (text + "Buzz");
+    }
+}
+
+const rule7 : Rule =
+{
+    rule_id : 7,
+
+    isConditionTrue(n : number) : boolean
+    {
+        return (n % 7 == 0);
+    },
+
+    apply(text : string) : string
+    {
+        return (text + "Bang");
+    }
+}
+
+const rule11 : Rule =
+{
+    rule_id : 11,
+
+    isConditionTrue(n : number) : boolean
+    {
+        return (n % 11 == 0);
+    },
+
+    apply(text : string) : string
+    {
+        return ("Bong");
+    }
+}
+
+const rule13 : Rule =
+{
+    rule_id : 13,
+
+    isConditionTrue(n : number) : boolean
+    {
+        return (n % 13 == 0);
+    },
+
+    apply(text : string) : string
+    {
+        const firstB : number = text.indexOf("B");
+
+        if(firstB == -1) return (text + "Fezz");
+
+        const fs : string = text.substr(0, firstB);
+        const bs : string = text.substr(firstB);
+        return (fs + "Fezz" + bs)
+    }
+}
+
+const rule17 : Rule =
+{
+    rule_id : 17,
+
+    isConditionTrue(n : number) : boolean
+    {
+        return (n % 17 == 0);
+    },
+
+    apply(text : string) : string
+    {
+        const fs : string[] = text.split('F');
+
+        let k : number;
+        for(k = 1; k < fs.length; k++)
+        {
+            fs[k] = "F" + fs[k];
+        }
+
+        const bs : string[] = fs.pop().split('B');
+
+        for(k = 1; k < bs.length; k++)
+        {
+            bs[k] = "B" + bs[k];
+        }
+
+        let reversedText : string = "";
+
+        for(k = 1; k < fs.length; k++)
+        {
+            reversedText = fs[k] + reversedText;
+        }
+        for(k = 0; k < bs.length; k++)
+        {
+            reversedText = bs[k] + reversedText;
+        }
+
+        return reversedText;
+    }
+}
+
+const rules : Rule[] = [rule3, rule5, rule7, rule11, rule13, rule17];
+
+for(i = 1; i <= 100; i++) //255255 applies all rules
 {
     text = "";
 
-    fezz = "";
-
-    if(i % 17 == 0) //reverse order
+    let j : number;
+    for(j = 0; j < rules.length; j++)
     {
-
-        if(i % 7 == 0)
-            text += "Bang";
-
-        if(i % 5 == 0)
-            text += "Buzz";
-
-        if(i % 13 == 0)
+        if(rules[j].isConditionTrue(i))
         {
-            text += "Fezz";
-            fezz = "Fezz";
+            text = rules[j].apply(text);
         }
-
-        if(i % 3 == 0)
-            text += "Fizz";
-
-        //11 is still last as it overwrites everything
-        if(i % 11 == 0)
-            text = fezz + "Bong";
-    }
-    else
-    {
-        if (i % 3 == 0)
-            text += "Fizz";
-
-        if(i % 13 == 0) //set fezz to be added in case of overwrite
-        {
-            text += "Fezz";
-            fezz = "Fezz";
-        }
-
-        if (i % 5 == 0)
-            text += "Buzz";
-
-        if (i % 7 == 0)
-            text += "Bang";
-
-        if(i % 11 == 0) //overwrite an add fezz if 13
-            text = fezz + "Bong";
     }
 
     if(text == "")
---
+++ indexBackup.ts
@@ -0,0 +1,62 @@
+let i : number;
+let text : string;
+let fezz : string;
+
+for(i = 1; i < 1000; i++)
+{
+    text = "";
+
+    fezz = "";
+
+    if(i % 17 == 0) //reverse order
+    {
+
+        if(i % 7 == 0)
+            text += "Bang";
+
+        if(i % 5 == 0)
+            text += "Buzz";
+
+        if(i % 13 == 0)
+        {
+            text += "Fezz";
+            fezz = "Fezz";
+        }
+
+        if(i % 3 == 0)
+            text += "Fizz";
+
+        //11 is still last as it overwrites everything
+        if(i % 11 == 0)
+            text = fezz + "Bong";
+    }
+    else
+    {
+        if (i % 3 == 0)
+            text += "Fizz";
+
+        if(i % 13 == 0) //set fezz to be added in case of overwrite
+        {
+            text += "Fezz";
+            fezz = "Fezz";
+        }
+
+        if (i % 5 == 0)
+            text += "Buzz";
+
+        if (i % 7 == 0)
+            text += "Bang";
+
+        if(i % 11 == 0) //overwrite an add fezz if 13
+            text = fezz + "Bong";
+    }
+
+    if(text == "")
+    {
+        console.log(i);
+    }
+    else
+    {
+        console.log(text);
+    }
+}
